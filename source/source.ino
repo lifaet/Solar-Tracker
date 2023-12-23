9162def2549a8f79b5b80adb060923603cded51b
lifaet <lifaethossain@gmail.com>
2023-12-23 11:20:17 +0600

    init

---
+++ source/source.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+#define ldr1 A0
+#define ldr2 A1
+Servo myservo;
+int pos = 90;
+int tolerance = 20;
+
+void setup() {
+  myservo.attach(4);
+  pinMode(ldr1, INPUT);
+  pinMode(ldr2, INPUT);
+  myservo.write(pos);
+  delay(1000);
+}
+
+void loop() {
+  int val1 = analogRead(ldr1);
+  int val2 = analogRead(ldr2);
+
+  if ((abs(val1 - val2) <= tolerance) || (abs(val2 - val1) <= tolerance)) {
+  } else {
+    if (val1 > val2) {
+      pos = pos + 1;
+    }
+    if (val1 < val2) {
+      pos = pos - 1;
+    }
+  }
+
+  if (pos > 180) {
+    pos = 180;
+  }
+  if (pos < 0) {
+    pos = 0;
+  }
+  myservo.write(pos);
+  delay(50);
+}
