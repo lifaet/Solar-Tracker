4bea3b0fa16f939dcecdac32294cde365bfadee3
lifaet <lifaethossain@gmail.com>
2023-12-28 20:09:07 +0600

    Update source.ino

--- source/source.ino
+++ source/source.ino
@@ -2,14 +2,12 @@
 // Servo to pin D4
 // LDR 1 to A0
 // LDR 2 to A1
-
 #include <Servo.h>
 #define ldr1 A0
 #define ldr2 A1
 Servo myservo;
 int pos = 90;
 int tolerance = 20;
-
 void setup() {
   myservo.attach(4);
   pinMode(ldr1, INPUT);
@@ -17,12 +15,10 @@ void setup() {
   myservo.write(pos);
   delay(1000);
 }
-
 void loop() {
   int val1 = analogRead(ldr1);
   int val2 = analogRead(ldr2);
   int lightThreshold = 50;
-
   if (val1 < lightThreshold && val2 < lightThreshold) {
     pos = 90;
   } else if ((abs(val1 - val2) <= tolerance) || (abs(val2 - val1) <= tolerance)) {
